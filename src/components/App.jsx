7ac01271cfac4e3877ef87ede8f6c647961d1063
Hyung Lee and Meguru Yasuda <meguru@>
2018-11-14 10:31:02 -0800

    finish linting

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+function App(){
+  return (
+    <div>
+        hello world!
+    </div>
+  );
+}
+
+export default App;
